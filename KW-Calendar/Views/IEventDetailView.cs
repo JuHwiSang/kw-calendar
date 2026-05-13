32981cc54c428edd62a7514476b22e0c1d1188ff
JuHwiSang <juhs2005@gmail.com>
2026-05-13 09:55:42 +0900

    feat: add EventDetailPresenter

--- KW-Calendar/Views/IEventDetailView.cs
+++ KW-Calendar/Views/IEventDetailView.cs
@@ -6,7 +6,7 @@ public interface IEventDetailView
 {
     // --- Presenter가 설정 ---
     Event CurrentEvent { get; set; }
-    bool IsFavorited { get; set; }
+    bool IsFavorited { get; set; } // TODO: Event 모델에 IsFavorited 속성이 존재하므로 제거 가능할 듯함
 
     // --- View가 발생, Presenter가 구독 ---
     event EventHandler<int> FavoriteToggleRequested;    // payload: event Id
